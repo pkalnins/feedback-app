86810a03e4aaca7e2a89d87b3864c3725356b2ed
Paul Kalnins <paulkalnins@Pauls-iMac.local>
2021-12-10 10:30:30 -0800

    Removed temporary data file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from "./components/Header"
 import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
